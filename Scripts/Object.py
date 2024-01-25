586ce1ad889a56a0725b988a6015abc231f3d8a1
KurumiiiiDaKat <arashinosango@gmail.com>
2024-01-25 23:01:33 +0100

    J'ai mis le Pooler sous forme d'objet (POO), j'ai ajouté la taille d'un objet dans ses caractéristiques (elle peut être modifiée durant l'éxécution), j'ai ajouté le principe de layers et de notCollidable pour détecter les collisions et il y a mtn une ébauche de fonction pour les collisions (qui ne fait rien pour l'instant)

--- Scripts/Object.py
+++ Scripts/Object.py
@@ -1,31 +1,64 @@
+"""
+    Ce script déclare tous les types d'objets différents (classes) dans la POO.
+"""
 import pygame
+
+
+""" ================================================================================================================ """
+
 
 class GameObject:
     """ Cette classe contient toutes les infos 'récurrentes' des objets du jeu.
-        - position (Vector2): la position (les coordonnées) de l'objet.
-        - surface (Surface): la Surface de pygame (la texture) de l'objet.
         - active (bool): si l'objet doit être simulé ou s'il est actuellement désactivé.
+        - position (Vector2): la position (les coordonnées) de l'objet.
+        - size (Vector2): la taille de l'objet (en pixels).
+        - surface (Surface): la Surface de pygame (la texture) de l'objet.
+
         - mass (float): la masse de l'objet (0 si pas affecté par la gravité).
         - velocity (Vector2): le vecteur vitesse de l'objet.
         - gravity (float): la gravité actuelle de l'objet (utile pour calculer sa gravité à chaque frame).
+        - layer (int): un nombre permettant de mettre les objets dans des catégories. Est utile pour calculer si un
+                        objet devrait entrer en collision avec un autre ou non.
+        - notCollidable (list[int]): liste de tous les layers avec lesquels l'objet en question ne peut pas entrer
+                                        en collision (ex.: si notCollidable = [1,2], alors l'objet ne pourra pas
+                                        toucher les objets de layer 1 ou 2, et passera à travers eux).
     """
 
-    def __init__(self, _position: (int, int), _texturePath: str, _mass: float, _velocity: (int, int) = (0, 0), _active: bool = True):
+    def __init__(self, _position: (int, int), _size: (int, int), _texturePath: str, _mass: float, _layer: int, _notCollidable: [int], _velocity: (int, int) = (0, 0), _active: bool = True):
         """ __init__ est appelée quand on crée l'objet.
             Args :
                 - self: obligatoire pour les méthodes (fonctions d'objets).
-                - _position (list): la position de départ de l'objet.
+                - _position (couple (int, int)): la position de départ de l'objet.
+                - _size (couple (int, int)): la taille de l'objet.
                 - _texturePath (str): le path vers la texture de l'objet.
                 - _mass (float): la masse de l'objet.
+                - _layer (int): le layer de l'objet.
+                - _notCollidable (list[int]): liste des layers que l'objet ne pourra pas percuter (entrer en collision).
                 - _velocity (couple (int, int)): la vitesse initiale de l'objet.
                 - _active (bool): si l'objet est activé ou non.
         """
+        self.active = _active
         self.position = Vector2(_position[0], _position[1])
+        self.size = Vector2(_size[0], _size[1])
         self.surface = pygame.image.load(_texturePath).convert()
+        self.Resize(_size)  # Permet d'appliquer directement la taille de l'objet.
+
         self.mass = _mass
         self.velocity = Vector2(_velocity[0], _velocity[1])
-        self.active = _active
         self.gravity = 0
+        self.layer = _layer
+        self.notCollidable = _notCollidable
+
+    def Resize(self, size: (int, int)):
+        """ Permet de modifier la taille de l'objet.
+            Args :
+                - size (couple (int, int)): la nouvelle taille de l'objet en x et en y.
+        """
+        self.size = Vector2(size[0], size[1])
+        self.surface = pygame.transform.scale(self.surface, size)
+
+
+""" ================================================================================================================ """
 
 
 class Vector2:
@@ -81,3 +114,33 @@ class Vector2:
                 - (couple (int, int)): le tuple associé au vecteur.
         """
         return self.x, self.y
+
+
+""" ================================================================================================================ """
+
+
+class Pooler:
+    """ Cette classe permet de créer un pooler, c'est-à-dire un dictionnaire de la forme {nom : liste de GameObjects},
+    soit {str: [GameObject]}. L'objectif du pooler est de centraliser le stockage de tous les objets pour pouvoir
+    les itérer rapidement, d'avoir un stockage ordonné et de pouvoir libérer un peu de mémoire RAM (pas sûr de ça).
+    J'ai fait un objet spécial pour le pooler pour pouvoir lui mettre des méthodes (= des fonctions).
+        - main ({str: [GameObject]}): structure principale du pooler, un dictionnaire contenant tous les GameObjects.
+    """
+
+    def __init__(self, categories: [str]):
+        """ Crée l'objet.
+            Args :
+                - categories ([str]): le nom de chaque catégorie d'objet présente dans le pooler (ex.: ["Player",
+                                        "Wall", "Enemy"] pour séparer ces 3 types d'objets).
+        """
+        self.main = {}
+        for name in categories:
+            self.main[name] = []    # Initialise toutes les catégories comme ça {'nom_de_la_catégorie': []}.
+
+    def AddObject(self, gameObject: GameObject, category: str):
+        """ Permet d'ajouter un nouvel objet au pooler (et par extension dans le jeu).
+            Args :
+                - gameObject (GameObject): l'objet à ajouter au pooler.
+                - category (str): la catégorie dans laquelle ajouter l'objet.
+        """
+        self.main[category].append(gameObject)
--- Scripts/Physics.py
+++ Scripts/Physics.py
@@ -4,6 +4,16 @@ import Scripts.Constants as Constants
 
 G = 0.00001
 deltaTime = Constants.deltaTime
+mainPooler = Object.Pooler([])
+
+def SetPooler(pooler: {str: [Object.GameObject]}):
+    """ Permet de récupérer le pooler de main.py et de le copier. Comme l'objet Pooler est mutable (comme les listes),
+    modifier le pooler de main.py modifiera directement ce pooler aussi. Le pooler est utile pour détecter des collisions.
+        Args :
+            - pooler (Pooler): le pooler de main.py.
+    """
+    global mainPooler
+    mainPooler = pooler
 
 def ApplyPhysics(body: Object.GameObject):
     """ Fonction principale de Physics.py. Réalise tous les calculs physiques et déplace l'objet en conséquence.
@@ -13,13 +23,16 @@ def ApplyPhysics(body: Object.GameObject):
     # Gravité.
     ApplyGravity(body)
 
+    # Collisions.
+    ManageCollisions(body)
+
     # On déplace l'objet là où il devrait être en fonction de sa vélocité.
     # La formule c'est (x1, y1) = (x0, y0) + Dt * (Vx, Vy).
     body.position += body.velocity * deltaTime
 
 def ApplyGravity(body: Object.GameObject):
     """ Calcule la gravité de l'objet donné et change ses coordonnées directement en fonction du résultat.
-        La formule c'est Dx = 0.5 * g * (t1^2 - t0^2).
+    La formule c'est Dx = 0.5 * g * (t1^2 - t0^2).
         Args:
             - body (GameObject): GameObject concerné.
     """
@@ -33,3 +46,10 @@ def ApplyGravity(body: Object.GameObject):
     # se trouve vers les valeurs positives, donc on ajoute la valeur de la gravité au lieu de la soustraire.
     body.velocity += Object.Vector2(0, addVelocity)
     body.gravity += G
+
+def ManageCollisions(body: Object.GameObject):
+    """ Calcule et applique les collisions entre les différents objets.
+        Args :
+            - body (GameObject): le GameObject pour qui on doit vérifier les collisions.
+    """
+    return True
